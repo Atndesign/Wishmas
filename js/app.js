5d01511ba30dac53530ef3accb92e6d3fc344644
Kevin <Atnnetworkk@gmail.com>
2019-11-30 16:23:19 +0100

    Add animations to selection and next step trnsition

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
 //global
-
+var step1Container = document.querySelector(".step1");
+var step2Container = document.querySelector(".step2");
+var step3Container = document.querySelector(".step3");
+var step4Container = document.querySelector(".step4");
 
 //step 1
 var choices = document.querySelectorAll('.choice-img-container > img')
 var choiceArr = Array.from(choices);
 var imgLink;
-
+var nexBtn = document.getElementById("step1") 
 choices.forEach(img => {
     img.addEventListener("click", function(){
-        if(imgLink != null){
-            document.getElementById("step1").disabled = false
-        }
+        
         removeClass(choices,"active");
+        removeClass(choices,"checkedAnim");
         img.classList.add("active")
+        img.classList.add("checkedAnim");
         imgLink = img.src;
+        if(imgLink != null){
+            nexBtn.disabled = false
+        }
     })
+})
+nexBtn.addEventListener("click",function(){
+    step1Container.style.display = "none";
 })
 
 function removeClass(array,classname){
@@ -23,5 +32,7 @@ function removeClass(array,classname){
     });
 }
 //step 2
+
 //step 3
+
 //step 4
